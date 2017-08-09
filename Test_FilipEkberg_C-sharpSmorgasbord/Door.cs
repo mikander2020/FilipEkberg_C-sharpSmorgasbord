1e8ae58781ee294d6b6a549e82fea586448bc0e8
mikander <mikander@hotmail.com>
2017-08-09 12:42:49 +0200

    Initial commit

---
+++ Test_FilipEkberg_C-sharpSmorgasbord/Door.cs
@@ -0,0 +1,5 @@
+﻿namespace Test_FilipEkberg_C_sharpSmorgasbord {
+    class Door {
+
+    }
+}
---
+++ Test_FilipEkberg_C-sharpSmorgasbord/TestDoor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_FilipEkberg_C_sharpSmorgasbord {
+    [TestClass]
+    public class TestDoor {
+        [TestMethod]
+        public void TestTryOpenDoor() {
+            var door = new Door();
+        }
+    }
+}
